02c7c02e1f5e8dac6a18f9812ae0aa26ef458f36
smin0820 <97029953+smin0820@users.noreply.github.com>
2023-05-15 22:10:08 +0900

    style: AddTodo 스타일링

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { v4 as uuid4 } from 'uuid';
+import styles from './AddTodo.module.css'
 
 export default function AddTodo({ onAdd }) {
     const [text, setText] = useState('');
@@ -9,24 +10,25 @@ export default function AddTodo({ onAdd }) {
     const handleSubmit = (event) => {
         // 페이지가 리프레쉬 금지
         event.preventDefault();
-        // 새로 submit된 todo
-        onAdd({ id: uuid4(), text, status: 'active' });
-        // 입력칸 초기화
-        setText('');
         // todo에 빈칸이 포함 제거
         if(text.trim().length === 0) {
             return;
         }
+        // 새로 submit된 todo
+        onAdd({ id: uuid4(), text, status: 'active' });
+        // 입력칸 초기화
+        setText('');
     } 
   return (
-    <form onSubmit={handleSubmit}>
-        <input 
+    <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+            className={styles.input}
             type="text" 
             placeholder='할 일을 입력하시오'
             value={text}
             onChange={handleChange}
         />
-        <button>Add</button>
+        <button className={styles.button}>+</button>
     </form>
   );
 }
